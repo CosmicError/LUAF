8b85e45535fb9e58f1b2b67bf8d05d8b4df9531b
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-07-25 15:00:06 -0400

    Encryption Change
    Removed use of LUAFK, encryption changed to sha512, no longer need the cryptography module which was replaced by hashlib, sql support coming soon!

--- ELUAL.py
+++ ELUAL.py
@@ -4,58 +4,47 @@ ELUAL will return false if the account does not exist or the username/password i
 
 so in your code you could do for example
 
-if ulogin.login(username, password) == False:
+if login.userlogin(username, password) == False:
    result of false
-elif ulogin.login == True:
+elif login.userlogin == True:
    result of true
 '''
 
 import os
-from cryptography.fernet import Fernet
-from ELUAFK import key
+import hashlib
+from LUAFCONFIG import sql, encoding
 
-class ulogin:
-    global f
-    f = Fernet(key)
-    def login(username, password):
-      usern = username
-      passw = password
-      #removed the b' at the beginning of the encrypted username
-      #and password and removes the last '
-      try:
-         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-         path2 = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + os.sep + 'ELA' + os.sep
-         linedata1 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[0:1])
-         linedata2 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[1:2])
-      except FileNotFoundError:
-         return False
-      except FileExistsError:
-         return True
-      lineuser = str(f.decrypt(linedata1.encode()))
-      linepass = str(f.decrypt(linedata2.encode()))
-      fuser = lineuser[2:len(lineuser) - 1]
-      fpass = linepass[2:len(linepass) - 1]
-      try:
-         #gets the current path and makes a folder named ELA
-         path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + os.sep + 'ELA'
-         os.mkdir(path)
-      except FileExistsError:
-         print('working...')
-      if os.path.isfile(path + os.sep + username + '.txt') == True:
-         if usern == fuser and passw == fpass:
-            print("you did it!")
-            return True
-         else:
-            print('your username or password is incorrect')
+class login:
+    def userlogin(username, password):
+        else: 
+        encryptun = hashlib.sha512(username.encode())
+        encryptpn = hashlib.sha512(password.encode())
+        usernl = encryptun.hexdigest()
+        passwl = encryptpn.hexdigest()
+        try:
+            __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+            path2 = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/ELA/'
+            linedata1 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[0:1])
+            linedata2 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[1:2])
+        except FileNotFoundError:
             return False
-      else:
-         print('no user found')
-         return False
-
-   #clear the recorded decrypted information
-      lineuser = ""
-      linepass = ""
-      fuser = ""
-      fpass = ""
-      usern = ""
-      passw = "" 
+        except FileExistsError:
+            return True
+        fuser = linedata1[2:len(linedata1)-4]
+        fpass = linedata2[2:len(linedata2)-2]
+        try:
+            #gets the current path and makes a folder named ELA
+            path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/ELA'
+            os.mkdir(path)
+        except FileExistsError:
+            print('working...')
+        if os.path.isfile(path + '/' + username + '.txt') == True:
+            if usernl == fuser and passwl == fpass:
+                print("you did it!")
+                return True
+            else:
+                print('your username or password is incorrect')
+                return False
+        else:
+            print('no user found')
+            return False
