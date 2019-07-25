3f785802a476fe69ec9a6e97c4b71ca4f40ba500
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-07-25 15:17:12 -0400

    removed use of LUAFK
    removed use of luafk, changed encryption to sha512

--- ELUAR.py
+++ ELUAR.py
@@ -10,13 +10,16 @@ contains Illegal characters
 Started 3/5/2019
 Finished Alpha - 3/19/2019
 Finished Beta - 3/27/2019
-Current Version -  v1.2
+Last Updated - 7/25/19
+Current Version - v2
+Stability - Stable
 Supported Python Versions - 3.5.x
                             3.6.x
+                            3.7.x
 
 -- Known Bugs --
-1a) LUAR creating FIleNotFounError
-1b) LUAR creating FIleExistsError
+1a) LUAR creating FileNotFounError
+1b) LUAR creating FileExistsError
 
 -- Bug Reasons --
 1a) gives error because the program is creating the account registered
@@ -32,54 +35,41 @@ Supported Python Versions - 3.5.x
 
 
 import os
+import hashlib
 import time
-from cryptography.fernet import Fernet
-from ELUAFK import key
 
-class eregister:
-    global f
-    f = Fernet(key)
-
+class register:
     def reguser(username, password):
-        user = username
-        passw = password
-        uencode = user.encode()
-        pencode = passw.encode()
         #encrypts the username and password
-        encryptuname = f.encrypt(uencode)
-        encryptupass = f.encrypt(pencode) 
-        encryptn = str(encryptuname)
-        encryptp = str(encryptupass)
-        namelen = len(encryptn) - 1
-        passlen = len(encryptp) - 1
-        #removed the b' at the beginning of the encrypted username
-        #and password and removes the last '
-        rname = encryptn[2:namelen]
-        rpass = encryptp[2:passlen]
+        usern = hashlib.sha512(username.encode())
+        passw = hashlib.sha512(password.encode())
+        rname = usern.hexdigest()
+        rpass = passw.hexdigest()
         try:
             #gets the current path and makes a folder named LA
-            path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + os.sep + 'ELA'
+            path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/ELA'
             os.mkdir(path)
         except FileExistsError:
             print('working...')
-        if os.path.isfile(path + os.sep + username + '.txt') == False:
+        if os.path.isfile(path + '/' + username + '.txt') == False:
             if len(username) <= 24 and len(password) <= 40 and len(username) >=3 and len(password) >=1:
             #checks if username and password do not exceed the stated maximum and minimum
                 IllegalChars = ['$', '@', '/', '[', ']', '{', '}', '<', '>', '=', '+', '?', '#', '%', '^', '&', '*', '(', ')', '|', ';', ':', ',', '~', '`', '\\', "\'", '\"', '.', ' ']
                 if any(x in username for x in IllegalChars):
                     #checks that the username does not have any illegal characters
                     print('Your username cannot contain Illegal characters')
-                    print('it can contain ! though')
                 else:
                     #if everything is true or passes then it will create the account
                     arfile = open(os.path.join(path, str(username) + '.txt'), 'a')
                     wrfile = open(os.path.join(path, str(username) + '.txt'), 'w')
-                    wrfile.write(rname)
-                    arfile.write('\n' + rpass)
+                    wrfile.write(rname + '\n' + rpass)
                     wrfile.close()
                     time.sleep(3)
             else:
                 print('your username(3-24) or password(1-40) is too long or too short')
         else:
             print('Either your username exists or it is a system error')
-eregister.reguser('ABC', 'ab12')
+            
+            
+            
+            
