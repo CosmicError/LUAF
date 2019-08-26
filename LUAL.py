f6bac2b1e7bb514dc3657c7b3434c25dd027473e
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-08-26 07:02:57 -0400

    Final Update

--- LUAL.py
+++ LUAL.py
@@ -1,28 +1,28 @@
 '''
-LUAL or Encrypted Local User Account Login
+LUAL or Local User Account Login
 LUAL will return false if the account does not exist or the username/password is incorrect
 
 so in your code you could do for example
 
-if login.userlogin(username, password) == False:
+if lual.login(username, password) == False:
    result of false
-elif login.userlogin == True:
+elif lual.login == True:
    result of true
 '''
 
 import os
 import hashlib
+from LUAFCONFIG import sql, encoding
 
-class login:
-    def userlogin(username, password):
-        else: 
+class lual:
+    def login(username, password):
         encryptun = hashlib.sha512(username.encode())
         encryptpn = hashlib.sha512(password.encode())
         usernl = encryptun.hexdigest()
         passwl = encryptpn.hexdigest()
         try:
             __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-            path2 = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/ELA/'
+            path2 = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/LA/'
             linedata1 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[0:1])
             linedata2 = str(open(os.path.join(path2, username + '.txt'), 'r').readlines()[1:2])
         except FileNotFoundError:
@@ -32,8 +32,7 @@ class login:
         fuser = linedata1[2:len(linedata1)-4]
         fpass = linedata2[2:len(linedata2)-2]
         try:
-            #gets the current path and makes a folder named ELA
-            path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/ELA'
+            path = str(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))) + '/LA'
             os.mkdir(path)
         except FileExistsError:
             print('working...')
